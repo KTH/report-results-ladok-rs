a8c60ff05f6db71d8e3f3458fe63115fabb242ec
Rasmus Kaj <kaj@kth.se>
2019-04-30 14:55:01 +0200

    Implement pagination in get_submissions.
    This is the Canvas header-based pagination.

--- src/canvas/mod.rs
+++ src/canvas/mod.rs
@@ -84,16 +84,53 @@ impl Canvas {
     }
 
     pub fn get_submissions(&self, sis_id: &str) -> Result<Vec<Submission>, Error> {
-        // TODO: Filter on assignement id in request?
-        Ok(self
-            .client
-            .get(&format!(
-                "{}/courses/sis_course_id:{}/students/submissions?student_ids[]=all&per_page=100",
-                self.base_url, sis_id
-            ))
-            .bearer_auth(&self.auth_key)
-            .send()?
-            .error_for_status()?
-            .json()?)
+        let mut result = vec![];
+        let mut next_url = Some(format!(
+            "{}/courses/sis_course_id:{}/students/submissions?student_ids[]=all&per_page=100",
+            self.base_url, sis_id
+        ));
+        while let Some(url) = next_url {
+            let mut resp = self
+                .client
+                .get(&url)
+                .bearer_auth(&self.auth_key)
+                .send()?
+                .error_for_status()?;
+            next_url = resp
+                .headers()
+                .get("link")
+                .and_then(|h| h.to_str().ok())
+                .and_then(get_next_url);
+            result.append(&mut resp.json()?);
+            dbg!(result.len());
+        }
+        Ok(result)
     }
+}
+
+fn get_next_url(links: &str) -> Option<String> {
+    const END: &str = ">; rel=\"next\"";
+    for link in links.split(',') {
+        if link.starts_with('<') && link.ends_with(END) {
+            return Some(link[1..link.len() - END.len()].to_string());
+        }
+    }
+    None
+}
+
+#[test]
+fn test_get_next_url() {
+    assert_eq!(
+        get_next_url("<https://kth.test.instructure.com/api/v1/courses/7798/students/submissions?student_ids%5B%5D=all&page=first&per_page=100>; rel=\"current\",<https://kth.test.instructure.com/api/v1/courses/7798/students/submissions?student_ids%5B%5D=all&page=bookmark:WzY1OTU4MzZd&per_page=100>; rel=\"next\",<https://kth.test.instructure.com/api/v1/courses/7798/students/submissions?student_ids%5B%5D=all&page=first&per_page=100>; rel=\"first\""),
+        // TODO: Is %5B%5D ok here?  Or do we need do urldecode []?
+        Some("https://kth.test.instructure.com/api/v1/courses/7798/students/submissions?student_ids%5B%5D=all&page=bookmark:WzY1OTU4MzZd&per_page=100".to_string()),
+    )
+}
+
+#[test]
+fn test_get_next_url_0() {
+    assert_eq!(
+        get_next_url("<https://kth.test.instructure.com/api/v1/courses/7798/students/submissions?student_ids%5B%5D=all&page=first&per_page=100>; rel=\"current\",<https://kth.test.instructure.com/api/v1/courses/7798/students/submissions?student_ids%5B%5D=all&page=first&per_page=100>; rel=\"first\""),
+        None,
+    )
 }
